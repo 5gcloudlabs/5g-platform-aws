2c6bd47dbfb3360b6524ece5191e94b2c10c6455
Amr Baraka <amr_baraka@outlook.com>
2025-06-19 14:54:08 +0100

    renamed and updated paths for free5gc & ueransim scripts

---
+++ scripts/free5gc-cli.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo -e "Configure the PLMN-ID for your 5G Core Network (MCC + MNC).\n"
+read -p "1. Enter a 3-digit Mobile Country Code (MCC) [default: 208]: " mcc_
+read -p "2. Enter a 2-digit Mobile Network Code (MNC) [default: 93]: " mnc_
+
+
+# Set environment variables
+export mcc=$mcc_
+export mnc=$mnc_
+
+
+# Substitute variables
+envsubst < ../argocd/free5gc-app.base > ../argocd/free5gc-app.yml
+envsubst '$mcc,$mnc' < ./subscriber-provisioner.base > ./subscriber-provisioner.sh
+envsubst '$mcc,$mnc' < ./ueransim-cli.base > ./ueransim-cli.sh
+envsubst '$mcc,$mnc' < ../argocd/ueransim-app.base > ../argocd/ueransim-app.tmp
+
+# Add executable permissions to script:
+chmod u+x ./ueransim-cli.sh
+
+# Apply free5gc yml file:
+kubectl apply -f ../argocd/free5gc-app.yml
