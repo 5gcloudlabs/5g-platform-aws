090a7c17831a8aec329dc83d3a695500e44048ef
Amr Baraka <amr_baraka@outlook.com>
2025-08-21 21:06:50 +0100

    updated branch to trims

--- infrastructure/argocd_required_apps.tf
+++ infrastructure/argocd_required_apps.tf
@@ -12,7 +12,7 @@ spec:
   project: default
   source:
     repoURL: https://github.com/5g-cloud-labs/new-test-2.git
-    targetRevision: charts
+    targetRevision: trims
     path: argocd/required-apps
   destination:
     server: https://kubernetes.default.svc
