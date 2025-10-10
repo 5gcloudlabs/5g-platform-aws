9e5ae8c123c4a015c63ca1e97c7223f3946c5f6d
amrbaraka <amr_baraka@outlook.com>
2025-10-10 19:41:19 +0200

    added cluster-issuer & certificate resources

---
+++ infrastructure/k8s_cert-manager_resources.tf
@@ -0,0 +1,41 @@
+resource "kubectl_manifest" "cluster-issuer" {
+  depends_on = [helm_release.cert-manager]
+    yaml_body = <<EOF
+apiVersion: cert-manager.io/v1
+kind: ClusterIssuer
+metadata:
+  name: cluster-issuer
+spec:
+  acme:
+    server: https://acme-v02.api.letsencrypt.org/directory
+    privateKeySecretRef:
+      name: cluster-issuer-account-key
+    solvers:
+    - dns01:
+        cloudflare:
+          apiTokenSecretRef:
+            name: cloudflare-api-token-secret-cert-manager-ns
+            key: cloudflare-api-token
+    EOF
+}
+
+
+resource "kubectl_manifest" "tls-cert" {
+  depends_on = [helm_release.cert-manager]
+    yaml_body = <<EOF
+apiVersion: cert-manager.io/v1
+kind: Certificate
+metadata:
+  name: tls-cert
+  namespace: istio-system
+spec:
+  issuerRef:
+    name: cluster-issuer
+    kind: ClusterIssuer
+  secretName: tls-cert-secret
+  commonName: cloud-5g.io
+  dnsNames:
+    - "cloud-5g.io"
+    - "*.cloud-5g.io"
+EOF
+}
