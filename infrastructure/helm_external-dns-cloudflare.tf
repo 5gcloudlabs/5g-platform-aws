f8ff38a0c3ce97914ec13b4def3aa4ae53bdd3ed
amrbaraka <amr_baraka@outlook.com>
2025-10-10 19:38:59 +0200

    updated helm release version

--- infrastructure/helm_external-dns-cloudflare.tf
+++ infrastructure/helm_external-dns-cloudflare.tf
@@ -5,7 +5,7 @@ resource "helm_release" "external-dns" {
   name       = "external-dns"
   repository = "https://kubernetes-sigs.github.io/external-dns/"
   chart      = "external-dns"
-  version    = "1.18.0"
+  version    = "1.19.0"
   namespace  = "kube-system"
 
   
