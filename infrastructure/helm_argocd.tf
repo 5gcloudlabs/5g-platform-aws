b8aff318eaec69daa34675d67c37b2a950bb33cb
Amr Baraka <amr_baraka@outlook.com>
2026-01-24 12:31:35 +0000

    added cmp sidecar config to argocd helm install

--- infrastructure/helm_argocd.tf
+++ infrastructure/helm_argocd.tf
@@ -1,14 +1,91 @@
 resource "helm_release" "argocd" {
-  depends_on = [kubernetes_namespace_v1.argocd, helm_release.aws-load-balancer-controller]  
+  depends_on = [ aws_ssm_association.ssm_association_5gcp_node ]
   name       = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
   version    = "8.2.5"
-  namespace  = kubernetes_namespace_v1.argocd.metadata[0].name
+  namespace  = "argocd"
+  create_namespace = true
+  wait                  = true
+  timeout               = 300
+  atomic                = false
+  render_subchart_notes = true
+  values = [
+    yamlencode({
+      configs = {
+        params = {
+          "server.insecure" = true
+        }
 
-  set = [{
-    name  = "configs.params.server\\.insecure"
-    value = "true"
-  }]
+        cmp = {
+          create = true
 
+          plugins = {
+            envsubst = {
+              generate = {
+                command = ["sh", "-c"]
+                args = [
+                  <<-EOF
+                  set -e
+                  find . -type f -name '*.yaml' -o -name '*.yml' | while read -r file; do
+                   echo "---"
+                    envsubst < "$file"
+                     done
+                  EOF
+                ]
+              }
+            }
+          }
+        }
+      }
+
+      repoServer = {
+        extraContainers = [
+          {
+            name    = "cmp-envsubst"
+            image   = "ghcr.io/5g-cloud-labs/argocd-cmp-envsubst/argocd-cmp-envsubst:v1"
+            command = ["/var/run/argocd/argocd-cmp-server"]
+
+            securityContext = {
+              runAsNonRoot = true
+              runAsUser    = 999
+            }
+
+            volumeMounts = [
+              {
+                name      = "var-files"
+                mountPath = "/var/run/argocd"
+              },
+              {
+                name      = "plugins"
+                mountPath = "/home/argocd/cmp-server/plugins"
+              },
+              {
+                name      = "argocd-cmp-cm"
+                mountPath = "/home/argocd/cmp-server/config/plugin.yaml"
+                subPath   = "envsubst.yaml"
+              },
+              {
+                name      = "cmp-tmp"
+                mountPath = "/tmp"
+              }
+            ]
+          }
+        ]
+
+        volumes = [
+          {
+            name = "argocd-cmp-cm"
+            configMap = {
+              name = "argocd-cmp-cm"
+            }
+          },
+          {
+            name     = "cmp-tmp"
+            emptyDir = {}
+          }
+        ]
+      }
+    })
+  ]
 }
