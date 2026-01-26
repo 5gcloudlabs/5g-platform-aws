80afa7fb03b276547b52fcc6af35a593bc674757
Amr Baraka <amr_baraka@outlook.com>
2026-01-26 21:14:48 +0000

    commented subnet cidr reservations

--- infrastructure/aws_vpc_multus-subnet.tf
+++ infrastructure/aws_vpc_multus-subnet.tf
@@ -14,11 +14,11 @@ resource "aws_subnet" "amf-N2-subnet" {
 }
 
 #explicit subnet CIDR reservation
-resource "aws_ec2_subnet_cidr_reservation" "amf-N2-subnet" {
-  cidr_block       = "100.64.1.0/28"
-  reservation_type = "explicit"
-  subnet_id        = aws_subnet.amf-N2-subnet.id
-}
+#resource "aws_ec2_subnet_cidr_reservation" "amf-N2-subnet" {
+#  cidr_block       = "100.64.1.0/28"
+#  reservation_type = "explicit"
+#  subnet_id        = aws_subnet.amf-N2-subnet.id
+#}
 
 
 
@@ -37,11 +37,11 @@ resource "aws_subnet" "smf-N4-subnet" {
 }
 
 #explicit subnet CIDR reservation
-resource "aws_ec2_subnet_cidr_reservation" "smf-N4-subnet" {
-  cidr_block       = "100.64.4.0/28"
-  reservation_type = "explicit"
-  subnet_id        = aws_subnet.smf-N4-subnet.id
-}
+#resource "aws_ec2_subnet_cidr_reservation" "smf-N4-subnet" {
+#  cidr_block       = "100.64.4.0/28"
+#  reservation_type = "explicit"
+#  subnet_id        = aws_subnet.smf-N4-subnet.id
+#}
 
 
 ###############################################
@@ -61,11 +61,11 @@ resource "aws_subnet" "free5gc-upf-N6-subnet" {
 }
 
 #explicit subnet CIDR reservation
-resource "aws_ec2_subnet_cidr_reservation" "upf-N6-subnet" {
-  cidr_block       = "100.64.6.0/28"
-  reservation_type = "explicit"
-  subnet_id        = aws_subnet.free5gc-upf-N6-subnet.id
-}
+#resource "aws_ec2_subnet_cidr_reservation" "upf-N6-subnet" {
+#  cidr_block       = "100.64.6.0/28"
+#  reservation_type = "explicit"
+#  subnet_id        = aws_subnet.free5gc-upf-N6-subnet.id
+#}
 
 
 
@@ -83,11 +83,11 @@ resource "aws_subnet" "free5gc-upf-N4-subnet" {
 
 
 #explicit subnet CIDR reservation
-resource "aws_ec2_subnet_cidr_reservation" "upf-N4-subnet" {
-  cidr_block       = "100.64.5.0/28"
-  reservation_type = "explicit"
-  subnet_id        = aws_subnet.free5gc-upf-N4-subnet.id
-}
+#resource "aws_ec2_subnet_cidr_reservation" "upf-N4-subnet" {
+#  cidr_block       = "100.64.5.0/28"
+#  reservation_type = "explicit"
+#  subnet_id        = aws_subnet.free5gc-upf-N4-subnet.id
+#}
 
 
 #create subnet for gNB n3 interface
@@ -104,11 +104,11 @@ resource "aws_subnet" "ueransim-gnb-N3-subnet" {
 
 
 #explicit subnet CIDR reservation
-resource "aws_ec2_subnet_cidr_reservation" "gnb-N3-subnet" {
-  cidr_block       = "100.64.2.0/28"
-  reservation_type = "explicit"
-  subnet_id        = aws_subnet.ueransim-gnb-N3-subnet.id
-}
+#resource "aws_ec2_subnet_cidr_reservation" "gnb-N3-subnet" {
+#  cidr_block       = "100.64.2.0/28"
+#  reservation_type = "explicit"
+#  subnet_id        = aws_subnet.ueransim-gnb-N3-subnet.id
+#}
 
 #create subnet for UPF n3 interface
 resource "aws_subnet" "upf-N3-subnet" {
@@ -124,11 +124,11 @@ resource "aws_subnet" "upf-N3-subnet" {
 
 
 #explicit subnet CIDR reservation
-resource "aws_ec2_subnet_cidr_reservation" "upf-N3-subnet" {
-  cidr_block       = "100.64.3.0/28"
-  reservation_type = "explicit"
-  subnet_id        = aws_subnet.upf-N3-subnet.id
-}
+#resource "aws_ec2_subnet_cidr_reservation" "upf-N3-subnet" {
+#  cidr_block       = "100.64.3.0/28"
+#  reservation_type = "explicit"
+#  subnet_id        = aws_subnet.upf-N3-subnet.id
+#}
 
 
 
@@ -146,8 +146,8 @@ resource "aws_subnet" "ueransim-gnb-N2-subnet" {
 }
 
 #explicit subnet CIDR reservation
-resource "aws_ec2_subnet_cidr_reservation" "gnb-N2-subnet" {
-  cidr_block       = "100.64.0.0/28"
-  reservation_type = "explicit"
-  subnet_id        = aws_subnet.ueransim-gnb-N2-subnet.id
-}
+#resource "aws_ec2_subnet_cidr_reservation" "gnb-N2-subnet" {
+#  cidr_block       = "100.64.0.0/28"
+#  reservation_type = "explicit"
+#  subnet_id        = aws_subnet.ueransim-gnb-N2-subnet.id
+#}
